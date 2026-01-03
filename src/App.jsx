047d0d92ab15ace3bcda22bcce9de2e92dcb882a
MKatay <kmagdi@kkando.hu>
2026-01-03 21:55:28 +0100

    AddTask

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { AddTask } from './components/AddTask'
 
 
 function App() {
   return (
     <div>
       <h1>Csapat projekt!</h1>
+      <AddTask/>
     </div>
   )
 }
