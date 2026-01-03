69cf5e9e5730d2b8b517ab9762cf4b982597080a
MKatay <kmagdi@kkando.hu>
2026-01-03 22:39:05 +0100

    delete form

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -4,10 +4,7 @@ export const AddTask = () => {
   return (
     <div>
       <h3>Új task hozzáadása</h3>
-      <form >
-        <input type="text" placeholder='új task...' />
-        <input type="submit" value="mentés" />
-      </form>
+    
     </div>
   )
 }
