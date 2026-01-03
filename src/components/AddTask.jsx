b62431cf3a89904143c66927b13494852150e3a6
MKatay <kmagdi@kkando.hu>
2026-01-03 22:43:35 +0100

    placeholder

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
 export const AddTask = () => {
+
   return (
     <div>
       <h3>Új task hozzáadása</h3>
       <form >
         <input type="text" placeholder='új task...' />
-         <input type="text" placeholder='hozzárendelve...' />
+         <input type="text" placeholder='kihez lesz hozzárendelve...' />
         <input type="submit" value="mentés" />
       </form>
     </div>
