c5a4f5424abeea01b8c420d1053b2addf2b24221
MKatay <kmagdi@kkando.hu>
2026-01-03 22:30:10 +0100

    h1 tag

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export const AddTask = () => {
   return (
     <div>
-      Új task hozzáadása
-      <form action="">
+      <h1>Új task hozzáadása</h1>
+      <form >
         <input type="text" placeholder='új task...' />
         <input type="submit" value="mentés" />
       </form>
