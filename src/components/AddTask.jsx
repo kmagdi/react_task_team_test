8aaf511a4fc97729cab4400f21dbededaf023dd1
MKatay <kmagdi@kkando.hu>
2026-01-03 22:12:07 +0100

    owner:form-input

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -4,6 +4,10 @@ export const AddTask = () => {
   return (
     <div>
       Új task hozzáadása
+      <form action="">
+        <input type="text" placeholder='új task...' />
+
+      </form>
     </div>
   )
 }
